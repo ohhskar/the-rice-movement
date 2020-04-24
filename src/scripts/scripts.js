1146104790d6eff18e6de2351ed6d7bc67e7134d
Oscar Vian Valles <oscarvianvalles@gmail.com>
2020-04-25 00:24:58 +0800

    working toggle

--- src/scripts/scripts.js
+++ src/scripts/scripts.js
@@ -1,1 +1,53 @@
-// Add your scripts here
+import { gsap } from 'gsap';
+
+class BankToggler {
+  constructor(element) {
+    this.parent = element;
+    this.toggler = element.querySelector('.bank__view');
+    this.body = element.querySelector('.bank__body');
+    this.defaultDuration = 0.25;
+    this.defaultEase = 'power1.out';
+  }
+
+  toggle() {
+    if (this.toggler.classList.contains('bank__view--hide')) {
+      gsap.timeline().to(this.body, {
+        duration: this.defaultDuration,
+        ease: this.defaultEase,
+        marginTop: 0,
+        height: 0,
+        onComplete: () => {
+          this.toggler.textContent = '+ View Details';
+        },
+      });
+    } else {
+      gsap.timeline().to(this.body, {
+        duration: this.defaultDuration,
+        ease: this.defaultEase,
+        marginTop: this.vhToPx(3.125),
+        height: this.body.scrollHeight,
+        onComplete: () => {
+          this.toggler.textContent = '- Hide Details';
+        },
+      });
+    }
+
+    this.toggler.classList.toggle('bank__view--hide');
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  vhToPx(value) {
+    return (value / 100) * window.innerHeight;
+  }
+
+  load() {
+    this.toggler.addEventListener('click', () => this.toggle());
+  }
+}
+
+const initializeAnimation = () => {
+  const bankNodes = document.querySelectorAll('.bank');
+  const bankObjects = [...bankNodes].map((element) => new BankToggler(element));
+  bankObjects.forEach((bank) => bank.load());
+};
+document.addEventListener('DOMContentLoaded', initializeAnimation);
